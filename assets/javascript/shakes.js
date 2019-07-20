30eff4a71d0d26e3008165b0db08616aa2e3b538
Brad Evans <kaloba4r@gmail.com>
2019-07-19 21:25:00 -0400

    event listeners to html and main function callback

--- assets/javascript/shakes.js
+++ assets/javascript/shakes.js
@@ -1,58 +1,9 @@
 // JQuery: Document Ready //
 $(document).ready(function () {
 
-    // Global Variable -  Initial Values //
-    var lyricOutput = "";
-
-    function lyricRequest(artist, song){
-        //  API Call to Vagalume.com --  returns lyrics string 
-        jQuery.getJSON(
-            "https://api.vagalume.com.br/search.php"
-            + "?art=" + artist
-            + "&mus=" + song
-            + "&apikey==8dc59cc7fdedc734247c325ec2ac4b7f",
-
-            function (data) {
-                console.log(data);
-
-                var test = data.mus[0].text
-                var testTrim = test.trim()
-                console.log(typeof testTrim, 'should be string');
-                $('#trending').append("<div class = 'lyric'>" + testTrim + "</div>");
-                lyricOutput = testTrim.replace(/[\r\n]*/g, "")
-                console.log(lyricOutput, 'wow w o returns')
-
-                console.log(lyricOutput, 'lyric Output is a string!');
-            }).then(function () {
-        // API Calls to https://apilist.fun/api/shakespeare-translation-api
-        var userQuote =    ""// lyricOutput.substring(0, 50);
-        var queryURL = "https://api.funtranslations.com/translate/shakespeare.json?text=" + encodeURIComponent(userQuote)
-        console.log(userQuote);
-        $.ajax({
-            url: queryURL,
-            data: {},
-            cache: false,
-            type: "POST",
-            success: function (response) {
-                console.log(response, 'shaspeare json')
-                console.log(response.contents.translated, 'response from shakespeare API')
-            },
-            error: function (xhr) {
-                console.log(xhr)
-                }
-        });
-    });
-    };
-
-  //  Function Calls
-
-  lyricRequest("Beyonce", "Flawless");
 
 
-
-
-
-    //DATABASE ICEBOX FEATURE  //  Storage of Artist and SongName to Firebase  //
+    //Database Set Up - ICEBOX FEATURE  //  Storage of Artist and SongName to Firebase  //
     var firebaseConfig = {
         apiKey: "AIzaSyCsMczUUrLbOzNHExrxUMXaJARFdkLldSk",
         authDomain: "shakes-e2b51.firebaseapp.com",
@@ -61,34 +12,75 @@ $(document).ready(function () {
         storageBucket: "",
         messagingSenderId: "793021669681",
         appId: "1:793021669681:web:873f46e902cad6fc"
+        };
+    firebase.initializeApp(firebaseConfig);
+
+    // Initial Values //
+        var lyricOutput = "";
+        var database = firebase.database();
+        var artist = '';
+        var song = '';
+        var likeCount = 0;
+
+    /// *** MAIN FUNCTION ***/// called by submit btn eventlistener, and contains a promise to address asyncronous return of API#1
+    function lyricRequest(artist, song) {
+        //  First API Call to retrieve music lyrics based artist and song parameters
+        jQuery.getJSON(
+            "https://api.vagalume.com.br/search.php"
+            + "?art=" + artist
+            + "&mus=" + song
+            + "&apikey==8dc59cc7fdedc734247c325ec2ac4b7f",
+            function (data) {
+                console.log(data, 'lyric object');  // logs API JSON object from API call#1
+                var test = data.mus[0].text;
+                var testTrim = test.trim();
+                lyricOutput = testTrim.replace(/[\r\n]*/g, "")  // removes returns and output breaks
+            }).then(function () {
+                // Second API Call to translate music lyric into Shakespearean English -- Will not execute until first API completes
+                var userQuote = lyricOutput.substring(0, 1000);
+                var queryURL = "https://api.funtranslations.com/translate/shakespeare.json?text=" + encodeURIComponent(userQuote) + "&api_key=bCjn5kpx1Lialiqvaw_g7QeF"
+                $.ajax({
+                    url: queryURL,
+                    data: {},
+                    cache: false,
+                    type: "POST",
+                    success: function (response) {
+                        console.log(response, 'shakespeare translation object')  // logs API JSON object from API call#2
+                        translated = response.contents.translated
+                        console.log(translated)    // logs translation to console //
+                        $('#trending').append("<div class = 'lyric'>" + translated + "</div>");
+                    },
+                    error: function (xhr) {
+                        console.log(xhr)
+                    }
+                });
+            });
     };
 
-    firebase.initializeApp(firebaseConfig);
-    var database = firebase.database();
-    var artist = '';
-    var song = '';
-    var likeCount = 0;
-
+    //  User Interface -  event listeners,global execution callbacks
     $("#submit").on("click", function (event) {
         event.preventDefault();
-        artist = $("#artistName").val().trim();
         song = $("#songName").val().trim();
-        likeCount = '#button#';
-
-        // console.log(song, 'song sent to db');
-        // console.log(artist, 'artist sent to db');
-        // console.log(likeCount, 'likeCount sent to db');
-
-        var musicCover = {
+        artist = $("#artistName").val().trim();
+        likeCount = 1;
+        //Interaction with Remote Servers & HTML - callback to fire lyricRequest() main function
+        console.log(artist, song, 'are key UI parameters for cb function ***')
+        lyricRequest(artist, song)  //  This callback fires main function lyricRequest() with two arguments
+        // Interaction with Database
+        var musicObject = {
             artist: artist,
             song: song,
-            likeCount: likeCount
+            likeCount: likeCount,
+            timestamp: firebase.database.ServerValue.TIMESTAMP
         }
-        database.ref().push(musicCover);
-
-        refreshPage();
+        // Code for the push to firebase database
+        database.ref().push(
+            musicObject
+        );
+        location.reload()
     });
 
+    // Database Interface - Returns new logged instance and future likeCount increment
     database.ref().on("child_added", function (childSnapshot) {
         var returnArtist = childSnapshot.val().artist;
         var returnSong = childSnapshot.val().song;
@@ -96,74 +88,4 @@ $(document).ready(function () {
         // console.log(returnArtist, 'artist returned from db');
         // console.log(returnSong, 'song returned from db');
     });
-
-    var refreshPage = function () {
-        location.reload()
-    }
-
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   // API Calls to https://musicbrainz.org/doc/Cover_Art_Archive/API
-//   var queryURL4 = " https://musicbrainz.org/ws/2/artist/3414d446-735a-443c-931f-10634f57e5b9"
-//   var queryURL = "http://coverartarchive.org/release/76df3287-6cda-33eb-8e9a-044b5e15ffdd"
-//     var queryURL2 = "http://musicbrainz.org/ws/2/artist/?query=artist:usher%20AND%20country:US"
-//   $.ajax({
-//       url: queryURL4,
-//       dataType: "json",
-//       data: {},
-//       cache: false,
-//       type: "GET",
-//       contentType: "application/json",
-//       success: function (response) {
-//           console.log(response, 'response from covers API')
-
-//       },
-//       error: function (xhr) {
-//           console.log(xhr)
-//       }
-//   });
-
-
-
-
-  // API Calls to https://musicbrainz.org/doc/Cover_Art_Archive/API
-  //  Queen -- http://musicbrainz.org/artist/0383dadf-2a4e-4d10-a46a-e9e041da8eb3
-  // Usher --  http://musicbrainz.org/artist/3414d446-735a-443c-931f-10634f57e5b9
-
-//   var queryURL3 = "http://musicbrainz.org/ws/2/artist/?query=artist:usher%20AND%20country:US"
-//   $.ajax({
-//       url: queryURL3,
-//       dataType: "json",
-//       data: {},
-//       cache: false,
-//       type: "GET",
-//       contentType: "application/json",
-//       success: function (response) {
-//           console.log(response, 'response from covers API')
-
-
-//       },
-//       error: function (xhr) {
-//           console.log(xhr)
-//       }
-//   });
-
-  // var length = 30;
-    // var myString = lyricOutput;
-    // var myTruncatedString = myString.substring(0, length);
-    // console.log(myTruncatedString);
