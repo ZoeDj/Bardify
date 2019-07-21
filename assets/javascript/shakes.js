3f3eec48cbe83b0f4d14909725847401ed7553e5
Brad Evans <kaloba4r@gmail.com>
2019-07-21 09:41:25 -0400

    trending iterator added to firebase to track an array of artist/songs requested by all users in the real time database

--- assets/javascript/shakes.js
+++ assets/javascript/shakes.js
@@ -12,15 +12,16 @@ $(document).ready(function () {
         storageBucket: "",
         messagingSenderId: "793021669681",
         appId: "1:793021669681:web:873f46e902cad6fc"
-        };
+    };
     firebase.initializeApp(firebaseConfig);
 
     // Initial Values //
-        var lyricOutput = "";
-        var database = firebase.database();
-        var artist = '';
-        var song = '';
-        var likeCount = 0;
+    var lyricOutput = "";
+    var database = firebase.database();
+    var artist = '';
+    var song = '';
+    var likeCount = 0;
+    var trendingArray = [];
 
     /// *** MAIN FUNCTION ***/// called by submit btn eventlistener, and contains a promise to address asyncronous return of API#1
     function lyricRequest(artist, song) {
@@ -72,38 +73,36 @@ $(document).ready(function () {
 
     $("#random").on("click", function (event) {
         event.preventDefault();
-        var randomArray= [
-            {artist: "Ariana Grande", song:"7 rings"}, 
-            {artist: "Beyonce", song: "Flawless"}, 
-            {artist: "Imagine Dragons", song: "Thunder"}, 
-            {artist: "U2", song: "One"}, 
-            {artist: "Lady Gaga", song: "Shallow"},
-            {artist: "21 Pilots", song: "Stressed Out"},
-            {artist: "Clash", song: "Should I stay or should I go"},
-         ]
-         var randomNum = Math.floor(Math.random()*randomArray.length);
-         console.log(randomNum)
-         var randomArtist = randomArray[randomNum].artist;
-         var randomSong = randomArray[randomNum].song;
-         console.log(randomArtist, randomSong, 'are random UI parameters for cb function ***')
-         lyricRequest(randomArtist, randomSong)  //  This callback fires main function lyricRequest() with two arguments
-                 });
+        var randomArray = [
+            { artist: "Ariana Grande", song: "7 rings" },
+            { artist: "Beyonce", song: "Flawless" },
+            { artist: "Imagine Dragons", song: "Thunder" },
+            { artist: "U2", song: "One" },
+            { artist: "Lady Gaga", song: "Shallow" },
+            { artist: "21 Pilots", song: "Stressed Out" },
+            { artist: "Clash", song: "Should I stay or should I go" },
+        ]
+        var randomNum = Math.floor(Math.random() * randomArray.length);
+        console.log(randomNum)
+        var randomArtist = randomArray[randomNum].artist;
+        var randomSong = randomArray[randomNum].song;
+        console.log(randomArtist, randomSong, 'are random UI parameters for cb function ***')
+        lyricRequest(randomArtist, randomSong)  //  This callback fires main function lyricRequest() with two arguments
+    });
 
     // Database Functions & Listeners
-        function logDatabase(){
-         var musicObject = {
+    function logDatabase() {
+        var musicObject = {
             artist: artist,
             song: song,
             likeCount: likeCount,
             timestamp: firebase.database.ServerValue.TIMESTAMP
         }
-        // Code for the push to firebase database
-        database.ref().push(musicObject);
-            }
-         // Database Listener - Returns new logged instance and future likeCount increment
-        database.ref().on("child_added", function (childSnapshot) {
-        var returnArtist = childSnapshot.val().artist;
-        var returnSong = childSnapshot.val().song;
-            });
-
+        // database.ref().push(musicObject);  // Code for the push to firebase database
+        trendingArray.push(musicObject);  // Updates trending array with user request artist&song
+        console.log(trendingArray, "Trending Artist Array")
+        database.ref().push(trendingArray);  // Updates database with the new array
+        //  after each user submits artist choice, store the trending array to the database; then, with each UI app instance set the initial value equal to the database stored value.
+        // lastly iterate over the array and count each repeat occurrences to create a trending variable for each artist //
+    }
 });
